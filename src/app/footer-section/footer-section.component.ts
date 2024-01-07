92d79b4662145b02fc983fb54c8d439ebfe2db8d
sharozekkhan <sharozekkhan@gmail.com>
2024-01-08 03:33:49 +0500

    Added footer functionality

--- src/app/footer-section/footer-section.component.ts
+++ src/app/footer-section/footer-section.component.ts
@@ -12,4 +12,15 @@ export class FooterSectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openGithub(){
+    const gitHubProfileLink = 'https://github.com/Shehrozkhannn';
+    window.open(gitHubProfileLink, '_blank');
+  }
+
+  openLinkdin(){
+    const linkedInProfileUrl  = 'https://www.linkedin.com/in/sharoze-kkhan';
+    window.open(linkedInProfileUrl, '_blank');
+  }
+
+
 }
